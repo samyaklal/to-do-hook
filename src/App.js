dfbcd3938350a65c1c657c2975189481c350579d
Samyak Kumar Lal <samyak@IN-C02D5LNQMD6M.local>
2022-01-18 02:58:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const taskInput = useRef(null);
+
+  const addTask = () => {
+    if (taskInput.current.value === "") {
+      return;
+    }
+
+    setTasks([...tasks, taskInput.current.value]);
+    taskInput.current.value = "";
+  };
+
+  const addOnEnter = e => {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  };
+
+  const removeTask = task => setTasks(tasks.filter(item => item !== task));
+
+  const list = tasks.map(task => (
+    <li key={task}>
+      <span>{task}</span>
+      <button onClick={() => removeTask(task)}>Remove</button>
+    </li>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>To Do</h1>
       </header>
-    </div>
+      <main>
+        <section>
+          <input type="text" placeholder="Enter task" ref={taskInput} onKeyUp={addOnEnter} />
+          <button onClick={addTask}>Add</button>
+        </section>
+        <section>
+          <ul>{list}</ul>
+        </section>
+      </main>
+    </>
   );
 }
 
